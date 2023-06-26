4f4b1c021c068d1647905588719c522f10bde8e4
chertykovskij <chertykovskij@gmail.com>
2023-06-27 00:52:49 +0500

    Добавьте файлы проекта.

---
+++ SqlQuery.sql
@@ -0,0 +1,9 @@
+﻿SELECT 
+    p.ProductName AS 'Имя продукта',
+    ISNULL(c.CategoryName, 'Категория не указана') AS 'Имя категории'
+FROM 
+    Products p
+LEFT JOIN 
+    ProductCategoryLink pcl ON p.ProductID = pcl.ProductID
+LEFT JOIN 
+    Categories c ON pcl.CategoryID = c.CategoryID
