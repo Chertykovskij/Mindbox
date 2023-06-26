5aa2bc45bf10e443707585332002158044a5e125
Chertykovskij <132472718+Chertykovskij@users.noreply.github.com>
2023-06-27 00:59:46 +0500

    Add files via upload

---
+++ ShapeLibraryTests/CircleTests.cs
@@ -0,0 +1,28 @@
+﻿using ShapeLibrary.Calculations;
+using ShapeLibrary.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace ShapeLibraryTests
+{
+    public class CircleTests
+    {
+        [Fact]
+        public async void TestGetAreaFigure()
+        {
+            // Arrange
+            var circle = new Circle(5);
+
+            // Act
+            var area = await CalculationsGeometricShapeParameters.CalculateAreaShapeAsync(circle);
+
+            // Assert
+            Assert.Equal(Math.PI * Math.Pow(5, 2), area);
+        }
+    }
+}
